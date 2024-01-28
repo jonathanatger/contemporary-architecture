1fe855e564b070a470f853814dba3059a20a9268
Jonathan ATGER <jonathan.atger@gmail.com>
2024-01-28 21:13:57 +0100

    added tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>This is a Vite app with React in TS</h1>
+      <h1 className="text-5xl font-bold underline">
+        This is a Vite app with React in TS !
+      </h1>
     </>
   );
 }
