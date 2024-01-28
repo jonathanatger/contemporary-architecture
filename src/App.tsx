32840c24a25bc929a0e091b83fdb07e714f36016
Jonathan ATGER <jonathan.atger@gmail.com>
2024-01-28 20:42:10 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>This is a Vite app with React in TS</h1>
+    </>
+  );
+}
+
+export default App;
