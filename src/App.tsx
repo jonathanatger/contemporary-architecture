9af13e22ebbe27283a4170e050e0134df2022a2b
Jonathan ATGER <jonathan.atger@gmail.com>
2024-01-28 22:01:39 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import "./index.css";
+// import "./index.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">
-        This is a Vite app with React in TS !
-      </h1>
+      <div style={{ height: "100vh" }} className="flex flex-col bg-gray-600">
+        <h1 className="text-5xl font-italic underline text-green-300 border-green-300 italic decoration-green-700 decoration-4 underline-offset-2 leading-snug truncate p-10">
+          This is a Vite app with React in TS ! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Totam at nemo nobis. Totam rerum
+          inventore recusandae nihil expedita quam, molestias ex iusto obcaecati
+          reiciendis incidunt soluta voluptas ad laboriosam consequuntur.
+        </h1>
+        <p></p>
+        <h1 className="text-4xl font-italic underline text-green-300">
+          And this is a subtitle for the sake of it
+        </h1>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        specificgreen: "#465c38",
+      },
+    },
   },
   plugins: [],
 };
