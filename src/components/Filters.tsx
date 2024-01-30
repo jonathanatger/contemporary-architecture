930fbd3c80480f5fcf779ca354da1b5053e3f6bb
Jonathan ATGER <jonathan.atger@gmail.com>
2024-01-30 08:37:40 +0100

    added missing files

---
+++ src/components/Filters.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Filters = function () {
+  return (
+    <div className="w-full flex justify-center">
+      <select className="m-4  w-64">Option 1</select>
+      <select className="m-4  w-64">Option 1</select>
+      <select className="m-4  w-64">Option 1</select>
+    </div>
+  );
+};
---
+++ src/components/Map.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Map = function () {
+  return <div className="bg-green-400 w-64 h-64 m-4"></div>;
+};
