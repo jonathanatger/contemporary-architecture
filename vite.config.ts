4ea5296261aa2c5a3c106be177c8691c6d028494
Jonathan ATGER <jonathan.atger@gmail.com>
2024-02-22 12:04:54 +0100

    svgLoader vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgLoader from "vite-svg-loader";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgLoader()],
   // resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
